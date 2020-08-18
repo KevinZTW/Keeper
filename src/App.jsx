f65abf1fc53b656d401e49a413623d06c1221501
KevinZhang <38662781+kevinsurf@users.noreply.github.com>
2020-08-18 16:44:23 +0800

    update

--- src/App.jsx
+++ src/App.jsx
@@ -4,43 +4,56 @@ import Header from "./Header.jsx"
 import Footer from "./Footer.jsx"
 import Note from "./Note.jsx"
 import Card from "./Card.jsx"
-import Hook from "./Hook.jsx"
+
 
 
 var change = true ;
 
-
 function App() {
-    const [time, ntime] = useState(123)
-    var state = 123;
-    function update(){
-        if (state = 123){
-            var newTime = 234;
-            console.log(state)
-            state = 234;
-        }else { newTime = 345;
-        console.log("chane to 345")}
-        
-        
-        ntime(newTime);
-    }
-
     
+    let [state, cstate] = useState(true)
+    
+    var newTime =234;
+    
+    function updateMotivation(){
+        if (state){
+            state = false;
+            
+            console.log(state)
+            
+        }else { state = true ;
+            
+            console.log(state)}
+        
+        
+        cstate(state);
+    }
+    let [color, ccolor] = useState("white")
+    function handleMouseover(){
+        console.log("hihi")
+        color = "black";
+        ccolor(color);
+    }
+    function handleMouseout(){
+        color = "black";
+        ccolor(color);
+    }
     return (
     <div>
     <div className= "keeper">
         <Header />
-        {time}
-        <button onClick={update}>Get time</button>
+        
         <Note />
-        <Hook />
+        
+        
+        <button style={{backgroundColor:color}}onClick={updateMotivation}onMouseOut={handleMouseout} onMouseOver={handleMouseover}>SWITCH MOTIVATION</button>
     </div>
     
 
-    {change ? <div className = "motivation">
-        <Card name =""  url = "https://picsum.photos/id/235/500/300" />
-        <Card name = ""  url = "https://picsum.photos/id/230/500/300" />
-        <Card name = ""  url = "https://picsum.photos/id/266/500/300" />
+    {state ? <div className = "motivation">
+        <Card name =""  url = "https://picsum.photos/id/235/400/300" />
+        <Card name = ""  url = "https://picsum.photos/id/230/400/300" />
+        <Card name = ""  url = "https://picsum.photos/id/266/400/300" />
     </div> : null}
     </div>
     
